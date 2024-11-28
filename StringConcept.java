819c053a0393658ad4653c88d17cd678d5741858
Rajesh E <rajeshofficial8647@gmail.com>
2024-11-28 12:26:42 +0530

    day4

---
+++ StringConcept.java
@@ -0,0 +1,17 @@
+package day5;
+import java.util.Scanner;
+
+public class StringConcept {
+public static void main (String[]args){
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter the name");
+			String name=scan.nextLine();
+			System.out.println(name);
+			System.out.println(name.length());
+			System.out.println(name.substring(0, 5));
+			System.out.println(name.charAt(0));
+			
+			
+ 	
+}
+}
