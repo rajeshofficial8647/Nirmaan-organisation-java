d6679d5bd77e3901bb4b6c9ff25ebbaffebae8f1
Rajesh E <rajeshofficial8647@gmail.com>
2024-11-28 13:05:24 +0530

    day4
    Class and Objects

---
+++ Student.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class Student {
+	String name="";
+	int age;
+	int rollnumber;
+	String section="";
+	public static void main(String[]args) {
+ Student s1=new Student();
+ s1.name="Rajesh";
+ s1.age=20;
+ s1.rollnumber=212103979;
+ s1.section="b";
+ System.out.println(s1.name);
+ System.out.println(s1.age);
+ System.out.println(s1.rollnumber);
+ System.out.println(s1.section);
+}}
