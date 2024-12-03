4bb19b30148fc0be232679b0551c21b836db7964
Rajesh E <rajeshofficial8647@gmail.com>
2024-12-03 12:57:39 +0530

    day6

---
+++ vmr.java
@@ -0,0 +1,16 @@
+package day7;
+import java.util.Scanner;
+public class vmr {
+	public static void main(String[]args)
+	{ 
+		Scanner scan =new Scanner(System.in);
+				String mehana =scan.nextLine();
+		if(mehana.equals("dead"))
+		{
+			System.out.println("surya weds ramya");              
+		}else {
+			System.out.println("Surya weds mehana");
+		}
+	}
+
+}
