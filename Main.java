f34df843f0749e69e479ec13c08a8b86a8ed35bb
Rajesh E <rajeshofficial8647@gmail.com>
2024-12-04 11:55:12 +0530

    day7

---
+++ Main.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class Main {
+ public static void main(String[]args) {
+	 caculator calc=new caculator();
+ int Addition=calc. Addition(10,20);
+	 System.out.println(Addition);
+	 
+ int Subtraction=calc.Subtraction(10,20);
+	 System.out.println(Subtraction);
+	 
+ int Multiple=calc.Multiple(10,20);
+ 	 System.out.println(Multiple);
+ 	 
+ int Division=calc.Division(10,20);
+	 System.out.println(Division);
+ }
+}
+main
---
+++ caculator.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class caculator {
+	
+ int Addition(int a,int b) {
+	
+	 return a+b;
+ }
+  int Subtraction(int a,int b) {
+	  return a-b;
+  }
+  int Multiple(int a,int b) {
+	  return a*b;
+  }
+  int Division(int a,int b) {
+	  return a/b; 
+}
+}
+ 
